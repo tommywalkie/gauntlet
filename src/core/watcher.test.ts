99edf7d87077472d96b2ec5004b355a29ced479d
tommywalkie <tommywalkie@gmail.com>
2021-05-07 19:18:43 +0200

    Added tests for folder renames / deletions

--- src/core/watcher.test.ts
+++ src/core/watcher.test.ts
@@ -107,7 +107,7 @@ it('should be able to track file renames via Deno.rename()', async () => {
             Deno.renameSync(join(tempDirName, "/A.txt"), join(tempDirName, "/B.txt"));
         }
     }
-    
+
     expect(occuredEvents[0].kind).toBe('watch')
     expect(occuredEvents.filter(el => el.kind === 'create').length).toBe(2)
     expect(occuredEvents.filter(el => el.kind === 'remove').length).toBe(1)
@@ -144,6 +144,74 @@ it('should be able to track file renames via Visual Studio Code', async () => {
     expect(occuredEvents.filter(el => el.kind === 'create').length).toBe(2)
     expect(occuredEvents.filter(el => el.kind === 'remove').length).toBe(1)
     expect(occuredEvents.filter(el => el.kind === 'remove')[0].entry.name).toBe('A.txt')
+
+    Deno.removeSync(tempDirName, { recursive: true })
+})
+
+it('should be able to track non-empty folder deletions', async () => {
+    const tempDirName = join(Deno.cwd(), "/foo")
+    try { Deno.removeSync(tempDirName, { recursive: true }) } catch(e) {}
+    Deno.mkdirSync(tempDirName, { recursive: true })
+    const watcher = watchFs({ source: "./foo", fs: denoFs })
+    const occuredEvents = []
+
+    setTimeout(() => (watcher as any).return(), DELAY + 1000)
+
+    for await (const event of watcher) {
+        occuredEvents.push(event)
+        if (event.kind === 'watch') {
+            Deno.mkdirSync(join(tempDirName, "/bar/"), { recursive: true })
+            Deno.writeTextFileSync(join(tempDirName, "/bar/A.txt"), "Hello world\n")
+            Deno.writeTextFileSync(join(tempDirName, "/bar/B.txt"), "Hello world\n")
+        }
+        if (occuredEvents.length === 5) {
+            Deno.removeSync(join(tempDirName, "/bar/"), { recursive: true });
+        }
+    }
+
+    expect(occuredEvents.length).toBe(7)
+    expect(occuredEvents[0].kind).toBe('watch')
+    expect(occuredEvents[1].kind).toBe('create')
+    expect(occuredEvents[2].kind).toBe('create')
+    expect(occuredEvents[3].kind).toBe('modify')
+    expect(occuredEvents[4].kind).toBe('modify')
+    expect(occuredEvents[5].kind).toBe('remove')
+    expect(occuredEvents[6].kind).toBe('remove')
+
+    Deno.removeSync(tempDirName, { recursive: true })
+})
+
+it('should be able to track non-empty folder renames', async () => {
+    const tempDirName = join(Deno.cwd(), "/foo")
+    try { Deno.removeSync(tempDirName, { recursive: true }) } catch(e) {}
+    Deno.mkdirSync(tempDirName, { recursive: true })
+    const watcher = watchFs({ source: "./foo", fs: denoFs })
+    const occuredEvents = []
+
+    setTimeout(() => (watcher as any).return(), DELAY + 1000)
+
+    for await (const event of watcher) {
+        occuredEvents.push(event)
+        if (event.kind === 'watch') {
+            Deno.mkdirSync(join(tempDirName, "/bar/"), { recursive: true })
+            Deno.writeTextFileSync(join(tempDirName, "/bar/A.txt"), "Hello world\n")
+            Deno.writeTextFileSync(join(tempDirName, "/bar/B.txt"), "Hello world\n")
+        }
+        if (occuredEvents.length === 5) {
+            Deno.renameSync(join(tempDirName, "/bar/"), join(tempDirName, "/rab/"));
+        }
+    }
+
+    expect(occuredEvents.length).toBe(9)
+    expect(occuredEvents[0].kind).toBe('watch')
+    expect(occuredEvents[1].kind).toBe('create')
+    expect(occuredEvents[2].kind).toBe('create')
+    expect(occuredEvents[3].kind).toBe('modify')
+    expect(occuredEvents[4].kind).toBe('modify')
+    expect(occuredEvents[5].kind).toBe('remove')
+    expect(occuredEvents[6].kind).toBe('remove')
+    expect(occuredEvents[7].kind).toBe('create')
+    expect(occuredEvents[8].kind).toBe('create')
 
     Deno.removeSync(tempDirName, { recursive: true })
 })
